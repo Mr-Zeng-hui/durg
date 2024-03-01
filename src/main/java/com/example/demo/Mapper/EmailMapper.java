8059c32c4e020a4fdb9bfe2640277345a8023c7f
tanzhenxiang <974180266@qq.com>
2024-03-01 23:21:27 +0800

    调整

--- src/main/java/com/example/demo/Mapper/EmailMapper.java
+++ src/main/java/com/example/demo/Mapper/EmailMapper.java
@@ -2,6 +2,7 @@ package com.example.demo.Mapper;
 
 import com.example.demo.Model.Email;
 import com.example.demo.Model.HelloModel;
+import com.example.demo.Model.User;
 import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Component;
 
@@ -17,8 +18,9 @@ public interface EmailMapper {
     Email select(int id);
 
     // 更新 value
-    @Update("UPDATE durg_config SET value=#{value} WHERE id=#{id}")
-    int updateValue(Email email);
-
+    @Update("UPDATE durg_config SET id=#{email.id}, userName=#{email.userName} " +
+            ", emailPwd=#{email.emailPwd} , email=#{email.email} , template=#{email.template}" +
+            ", emailType=#{email.emailType}  WHERE id=#{email.id}")
+    int updateValue(@Param("email") Email email);
 
 }
